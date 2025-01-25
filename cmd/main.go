2899f5ba8f83f7b8b5d305d08cf73c0deeca9bf8
MgenGlder <mgen.glder@gmail.com>
2025-01-25 13:46:34 -0500

    fix: Update return type

--- cmd/main.go
+++ cmd/main.go
@@ -145,7 +145,7 @@ func main() {
 		}
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprint(w, "Registered.")
+		fmt.Fprint(w, "{ 'generated': true }")
 	})
 
 	// Handler function for the root path ("/")
